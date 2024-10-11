e4321a81803f50dde738b8c4de7037eef7ff70a4
08062080 <133919628+08062080@users.noreply.github.com>
2024-10-11 09:32:05 -0300

    b

---
+++ perguntas.js
@@ -0,0 +1,108 @@
+ criaCartao(
+    'Matematica',
+    'Qual o básico da Matemática?',
+    'Adição, subtração, multiplicação e divisão'
+)
+
+ criaCartao(
+    'Matematica',
+    'Qual é a questão mais difícil de Matemática?',
+    'A hipótese de Riemann'
+)
+
+ criaCartao(
+    'Matematica',
+    'Qual é o maior número do mundo?',
+    'googolplex'
+)
+ criaCartao(
+    'Matematica',
+    'Quem criou os números?',
+    'os números que usamos foram criados pelos indianos, no Norte da Índia, em meados do século V da era cristã'
+)
+ criaCartao(
+    'Matematica',
+    'Qual é o menor número que existe?',
+    'zero'
+)
+ criaCartao(
+    'Matematica',
+    'Quem inventou o número pi?',
+    'Arquimedes'
+)
+ criaCartao(
+    'Matematica',
+    'Por que pi é infinito?',
+    'porque é um número irracional.'
+)
+criaCartao(
+    'Matematica',
+    'Como calcular o raio?',
+    'O raio de um círculo é sempre a metade do diâmetro'
+)
+ criaCartao(
+    'Matematica',
+    'Porque o pi não tem fim?',
+    'Porque π é um número transcendente, a quadratura do círculo não é possível em um finito número de passos utilizando régua e compasso.'
+)
+criaCartao(
+    'Matematica',
+    'Qual a área do cilindro?',
+    '2πr(r + h)'
+)
+ criaCartao(
+    'Matematica',
+    'Como se mede o diâmetro?',
+    'posicione uma régua no meio do círculo e veja o comprimento marcado na borda. Agora, para saber como calcular o diâmetro de um círculo, multiplique esse resultado por dois'
+)
+ criaCartao(
+    'Matematica',
+    'Qual é a área da esfera?',
+    'o produto entre 4, o número irracional π e o raio R elevado ao quadrado.'
+)
+ criaCartao(
+    'Matematica',
+    ' Como calcular cone?',
+    ' A = π · r (r + g)'
+)
+ criaCartao(
+    'Matematica',
+    'Quem gritou Eureka?',
+    'Arquimedes de Siracusa'
+)
+ criaCartao(
+    'Matematica',
+    'Como medir mm?',
+    'O milímetro equivale a dividir o metro em 1000 partes iguais'
+)
+criaCartao(
+    'Matematica',
+    'Qual é o símbolo do diâmetro?',
+    ' ⌀'
+)
+criaCartao(
+    'Matematica',
+    'Qual é a circunferência da Terra?',
+    '40.075km'
+)
+criaCartao(
+    'Matematica',
+    'Como surgiu o cone?',
+    'O primeiro cone surgiu em 1896, em Nova York (Estados Unidos). O responsável pela invenção foi o italiano Italo Marchiony'
+)
+criaCartao(
+    'Matematica',
+    'Qual a distância do Sol?',
+    '149.600.000 km'
+)
+criaCartao(
+    'Matematica',
+    'O que é raio equatorial?',
+    'é a distância de seu centro ao equador e é igual a 6.378.137 m.'
+)
+
+
+
+
+
+//Ao todo são 20 perguntas de memorização do ensino médio
