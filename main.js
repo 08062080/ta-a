6cca57335dee2f2cc84725c633983b7487dfa922
08062080 <133919628+08062080@users.noreply.github.com>
2024-10-18 08:42:53 -0300

    y

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function criaCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
-    let cartao = document.creatElement('article');
+    let cartao = document.createElement('article');
     cartao.ClassName = 'cartao';
     cartao.innerHTML = `
     <div class="conteudo-cartao">
